964b05c46b3812ff8884672d1397dacb56e86dd4
muhammadnomanrehan <noman.rehan@systemsltd.com>
2026-01-21 22:22:00 -0800

    db.js updated

--- user-backend/config/db.js
+++ user-backend/config/db.js
@@ -1,15 +1,11 @@
-
 import mongoose from "mongoose";
-
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
   const dbName = process.env.DB_NAME || "user_app";
-
   if (!uri) {
     console.warn("MONGO_URI not set in .env. Skipping DB connection.");
     return;
   }
-
   try {
     await mongoose.connect(uri, { dbName });
     console.log("MongodB connected");
